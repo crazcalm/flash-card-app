380611c6a3deca3a65569f88a5b37227d31f841b
Marcus Willock <crazcalm@gmail.com>
2023-08-16 21:01:52 +0100

    Add ability to read CSV from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
 use std::fs::File;
+use std::path::PathBuf;
 
-use flash_cards::{Cards, Card};
-use flash_cards::traits::{FlashCard, FlashCards, FlipFlashCard, Loader};
+use clap::{Parser, Subcommand};
+
 use flash_cards::enums::FlashCardState;
 use flash_cards::loader::Csv;
+use flash_cards::traits::{FlashCard, FlashCards, FlipFlashCard, Loader};
+use flash_cards::{Card, Cards};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Loader commands
+    Loader {
+        /// lists test values
+        #[arg(long)]
+        csv: PathBuf,
+    },
+}
 
 fn main() {
-    let _test_data = "\
-front,back,hint,
-front_1,back_1,hint_1,
-front_2,back_2,hint_2,
-front_3,back_3,,
-";
+    let cli = Cli::parse();
+    let mut cards: Box<dyn FlashCards<Card>>;
 
-    // TODO: create a config the sets where the default directory path for csv files.
-    // Figure out path issuses so that it can be called from anywhere.
-    let test_file = File::open("./src/data/test_data.csv").unwrap();
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Loader { csv }) => {
+            let test_file = File::open(csv).unwrap();
 
-    let mut cards = Csv::load(test_file).unwrap();
-
-    let mut card: Card = cards.draw().unwrap();
-    card.set_state(FlashCardState::Hint);
-    println!("{}", card);
-    println!("{}", cards);
+            cards = Csv::load(test_file).unwrap();
+            let mut card: Card = cards.draw().unwrap();
+            card.set_state(FlashCardState::Hint);
+            println!("{}", card);
+            println!("{}", cards);
+        }
+        None => {}
+    }
 }
